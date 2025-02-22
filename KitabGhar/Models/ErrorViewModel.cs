e75719ea3631e2848e69c26254aafb3ef2d71623
rahul20610105 <rahul524327@gmail.com>
2025-02-22 11:48:10 +0545

    Add project files.

---
+++ KitabGhar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KitabGhar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
